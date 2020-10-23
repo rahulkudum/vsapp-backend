4d1be1c6b8ed583b5071513ce959a30c39237978
rahulkudum <rahulkudum@gmail.com>
2020-10-23 17:59:09 +0530

    boosk

--- app.js
+++ app.js
@@ -8,13 +8,59 @@ app.use(cors());
 
 
 
-//fs.copyFileSync("gita.txt","file.txt");
-var val="yjvj"; 
+//fs.copyFileSync("gita.txt","file.txt";
+let val; 
       
-fs.readFile("./gita.txt","utf8",function(error,data){
-   // console.log(data);
-    val=data;
-});   
+fs.readFile("./SB1.txt","utf8",function(error,data){
+  let sname=0;
+  let ntext="";
+  while(sname!==-1){
+  
+  sname=data.indexOf("Song",sname+5);
+  let tsname=data.indexOf("TEXT",sname+5);   
+ let ename=data.indexOf("Song",sname+5);
+ 
+ let vtext=data.slice(sname,ename);
+ ntext = ntext.concat("\n"+data.slice(sname,tsname));
+let tname=0;
+while(tname!==-1){
+   
+    tname=vtext.indexOf("TEXT",tname+5);
+    tename=vtext.indexOf("TEXT",tname+5)!==-1 ? vtext.indexOf("Text",tname+5) : ename;
+    let dtext=vtext.slice(tname,tename).split(/\r\n|\n/);
+    
+    let left=false;
+  let right=false;
+  for(let i in dtext){
+      
+      if(left){
+          if(dtext[i]!=="" && dtext[i]!==" ") right=true;
+      } 
+      if(dtext[i].indexOf(";")!==-1) left=true;
+      if(dtext[i]!=="" && dtext[i]!==" ") ntext=ntext.concat("\n"+dtext[i]);
+      if(left && right) break;
+      
+
+
+  }  
+
+}
+
+console.log(ntext);
+
+ 
+  
+  }  
+  
+  fs.writeFile("./SB1.txt",ntext, function (err) {
+    if (err) throw err;
+    console.log('Replaced!');
+  });
+   
+});
+
+
+
 //console.log(val);
 app.get ("/",(req,res)=>{
     res.send("chant and be happy");
