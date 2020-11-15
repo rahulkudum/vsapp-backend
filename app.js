aee29f238820cd5bf57d197b6213fc64673b2779
rahulkudum <rahulkudum@gmail.com>
2020-11-15 18:47:00 +0530

    books

--- app.js
+++ app.js
@@ -11,53 +11,87 @@ app.use(cors());
 
  let val; 
       
-// fs.readFile("./SBAll.txt","utf8",function(error,data){
-//   let sname=0;
-//   let ntext="";
-//   while(sname!==-1){
+fs.readFile("./SB-10.txt","utf8",function(error,data){
+  let sname=0;
+  let ntext="";
+  let cout=0;
+  while(sname!==-1){
   
-//   sname=data.indexOf("Song",sname+5);
-//   let tsname=data.indexOf("TEXT",sname+5);   
-//  let ename=data.indexOf("Song",sname+5);
+  sname=data.indexOf("Song",sname+5);
+  let tsname=data.indexOf("TEXT",sname+5);   
+ let ename=data.indexOf("Song",sname+5);
  
-//  let vtext=data.slice(sname,ename);
-//  ntext = ntext.concat("\n"+data.slice(sname,tsname));
-// let tname=0;
-// while(tname!==-1){
-   
-//     tname=vtext.indexOf("TEXT",tname+5);
-//     tename=vtext.indexOf("TEXT",tname+5)!==-1 ? vtext.indexOf("Text",tname+5) : ename;
-//     let dtext=vtext.slice(tname,tename).split(/\r\n|\n/);
+ let vtext=data.slice(sname,ename);
+ ntext = ntext.concat("\n"+data.slice(sname,tsname));
+let tname=0;
+while(tname!==-1){
+   cout++;
+    tname=vtext.indexOf("TEXT",tname+5);
+    tename=vtext.indexOf("TEXT",tname+5);
     
-//     let left=false;
-//   let right=false;
-//   for(let i in dtext){
-      
-//       if(left){
-//           if(dtext[i]!=="" && dtext[i]!==" ") right=true;
-//       } 
-//       if(dtext[i].indexOf(";")!==-1) left=true;
-//       if(dtext[i]!=="" && dtext[i]!==" ") ntext=ntext.concat("\n"+dtext[i]);
-//       if(left && right) break;
-      
+    let dumtext=vtext.slice(tname,tename).split(/\r\n|\n/);
+   
+    let left=false;
+  let right=false;
+let sl=0;
+let dutext="";
+
+for (let i in dumtext ){
+   
+    if(dumtext!=="" || dumtext!==" "  ){
+        dutext=dutext.concat("\n"+dumtext[i]);
+    }
+  
+   
+    
+}
+
+dtext=dutext.split(/\r\n|\n/);
 
 
-//   }  
 
-// }
+for (let i in dtext){
+    if(dtext[i].indexOf(";")!==-1) {sl=((i-2)/2)+2;
+    
+    
+     break;
+    
+    
+    }
+}
+
+
+
+if(tname!==-1){
+ntext=ntext.concat("\n"+"Text")};
+  for(let i=sl;i<dtext.length;i++){
+   
+      if(left){
+          if(dtext[i]!=="" && dtext[i]!==" ") right=true;
+      } 
+    
+      if(dtext[i].indexOf(";")!==-1) left=true;
+      if(dtext[i]!=="" && dtext[i]!==" " ) ntext=ntext.concat("\n"+dtext[i]);
+      if(left && right) break;
+    }
+
+
+   
+
+}
 
 
 
  
   
-//   }  
+  }  
   
-//   fs.writeFile("./SBnew.txt",ntext, function (err) {
-//     if (err) throw err;
-//     console.log('Replaced!');
-//   });
+  fs.writeFile("./SB10new.txt",ntext, function (err) {
+    if (err) throw err;
+    console.log('Replaced!');
+  });
    
-// });
+});
 
 
 
